f622e8a2bfccd76479510429295898bdac8f1617
MRK501 <manukhedekar501@gmail.com>
2024-07-11 11:37:29 +0530

    2nd commit

--- login/login.js
+++ login/login.js
@@ -12,9 +12,22 @@ signInBtnLink.addEventListener("click", () => {
 const togglepassword = document.querySelector('#togglepassword');
 const password = document.querySelector('#password');
 
-togglepassword.addEventListener('click', function (e) {
+/*togglepassword.addEventListener('click', function (e) {
   const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
   password.setAttribute('type', type);
 
   this.querySelector('i').classList.toggle('fa-eye');
-})
+})*/
+/*var a;
+function pass() {
+  if (a == 1)
+  {
+    document.getElementById('password').type = 'password';
+    document.getElementById('pass-icon').src = 'pass hide.png';
+  }
+  else {
+    document.getElementById('password').type = 'text';
+    document.getElementById('pass-icon').src = 'pass show.png';
+    a = 1;
+  }
+}*/
